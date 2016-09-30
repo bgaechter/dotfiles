ef994aeb8538ba9a4eeedb8ef2f4745b6f46c39f
bgaechter <benny.gaechter@gmail.com>
2016-09-30 23:00:52 +0000

    Added qtile conf

--- install.sh
+++ install.sh
@@ -2,18 +2,70 @@
 
 VUNDLE_DIR=~/.vim/bundle/Vundle.vim/
 AWESOME_DIR=~/.awesome
-LIBCLANG_INSTALLED=$(sudo ldconfig -p | grep libclang | wc -l)
-LIBBOOST_INSTALLED=$(sudo ldconfig -p | grep libboost | wc -l)
+QTILE_DIR=~/.config/qitle
+LIBCLANG_INSTALLED=$(ldconfig -p | grep libclang | wc -l)
+LIBBOOST_INSTALLED=$(ldconfig -p | grep libboost | wc -l)
 
-if [ "$LIBCLANG_INSTALLED" -eq 0 ]; then
-	echo "Please install libclang"
-	exit;
-fi
+function configure_awesome {
 
-if [ "$LIBBOOST_INSTALLED" -eq 0 ]; then
-	echo "Please install boost"
-	exit;
-fi
+	if [ ! -d "$AWESOME_DIR" ]; then
+		mkdir -p ~/.awesome/
+	fi
+	cp -i awesome/rc.lua ~/.awesome/rc.lua
+}
+
+function configure_qtile {
+
+	if [ ! -d "$QTILE_DIR" ]; then
+		mkdir -p ~/.awesome/
+	fi
+	cp -i qtile/config.py ~/.config/qtile/config.py
+}
+
+function install_ycm {
+
+	if [ "$LIBCLANG_INSTALLED" -eq 0 ]; then
+		echo "Please install libclang"
+		exit;
+	fi
+
+	if [ "$LIBBOOST_INSTALLED" -eq 0 ]; then
+		echo "Please install boost"
+		exit;
+	fi
+
+	# build YouCompleteMe
+	cd ~/.vim/bundle/YouCompleteMe
+	python2 install.py --clang-completer --system-libclang --system-boost
+}
+
+while true; do
+	read -p "Do you wish to set up YouCompleteMe using this script? `echo $'\n '`" yn
+	case $yn in
+		[Yy]* ) INSTALL_YCM=yes; break;;
+		[Nn]* ) INSTALL_YCM=no; break;;
+		* ) echo "Please answer yes or no."
+	esac
+done
+
+while true; do
+	read -p "Do you want to use qtile? `echo $'\n '`" yn
+	case $yn in
+		[Yy]* ) CONFIGURE_QTILE=yes; break;;
+		[Nn]* ) CONFIGURE_QTILE=no; break;;
+		* ) echo "Please answer yes or no."
+	esac
+done
+
+while true; do
+	read -p "Do you want to use awesome? `echo $'\n '`" yn
+	case $yn in
+		[Yy]* ) CONFIGURE_AWESOME=yes; break;;
+		[Nn]* ) CONFIGURE_AWESOME=no; break;;
+		* ) echo "Please answer yes or no."
+	esac
+done
+
 
 # get Vundle
 if [ ! -d "$VUNDLE_DIR" ]; then
@@ -27,16 +79,21 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # move the dotfiles
 cp -i vimrc ~/.vimrc
 cp -i tmux.conf ~/.tmux.conf
-if [ ! -d "$AWESOME_DIR" ]; then
-	mkdir -p ~/.awesome/
-fi
-cp -i awesome/rc.lua ~/.awesome/rc.lua
 cp -i zshrc ~/.zshrc 
 cp -i ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+if [ $CONFIGURE_QTILE = "yes" ]; then
+	configure_qtile
+fi
+
+if [ $CONFIGURE_AWESOME = "yes" ]; then
+	configure_awesome
+fi
+
+if [ $INSTALL_YCM = "yes" ]; then
+	install_ycm
+fi
 
 # install vim plugins
 vim +PluginInstall +qall
 
-# build YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-python2 install.py --clang-completer --system-libclang --system-boost
